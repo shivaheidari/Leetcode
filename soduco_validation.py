c210d164882d78bb38d4f6973263326c04a21d39
Shiva <shivaheydari.es@gmail.com>
2022-10-11 20:02:53 -0600

    Medium

--- soduco_validation.py
+++ soduco_validation.py
@@ -1,52 +1,53 @@
 def validation(board):
     valid=True
+
     #checking the rows
-    
     for i in range(0,9):
-        
         seen=set()
         for j in range(0,9):
-            print("Number",board[i][j])
-            if board[i][j]==".": 
-                continue
-            if not board[i][j] in seen:
-                seen.add(board[i][j])
-            else: 
-                return False
+            if board[i][j]!=".": 
+                
+               if not board[i][j] in seen:
+                  seen.add(board[i][j])
+               else: 
+                  return False
 
     #checking the columns 
-        for i in range(0,9):
+    for i in range(0,9):
             seen=set()
             for j in range(0,9):
-                  if board[j][i]==".": 
-                     continue
-            if not board[i][j] in seen:
-                seen.add(board[j][i])
-            else: 
-                return False
+              if board[j][i]!=".": 
+                if not board[j][i] in seen:
+                    seen.add(board[j][i])
+                    print("seen",seen)
+                else: 
+                    return False
+
         
-    #checking the squires
-        i_limit=0
-        for i in range(0,9,4):
+    #checking the squares
+    for i in range(0,9,3):
             print(i,"i")
-            for j in range(0,9,4):
+            for j in range(0,9,3):
                 print("j",j)
                 seen=set()
                 for it in range(0,3):
                     for jt in range(0,3):
-                        print("It",it,"jt",jt,"Number",board[i+it][j+jt])
-                        print("Seen",seen)
-                        if board[i+it][j+jt]==".":
-                            continue
-                        if board[i+it][j+jt] not in seen:
-                            print("in")
-                            seen.add(board[i+it][j+it])
-                        else: 
-                            return False
+                        number=board[i+it][j+jt]
+                        if number != ".": 
+                            if number not in seen:
+                                seen.add(number)
+                                print(seen)
+                            else: 
+                                 return False
 
 
     return valid
 
 
 
-print(validation([["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]))
+print(validation([[".",".","4",".",".",".","6","3","."],[".",".",".",".",".",".",".",".","."],["5",".",".",".",".",".",".","9","."],[".",".",".","5","6",".",".",".","."],["4",".","3",".",".",".",".",".","1"],[".",".",".","7",".",".",".",".","."],[".",".",".","5",".",".",".",".","."],[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".","."]]))
+def board_print(board):
+    for i in range(0,9):
+            print(board[i])
+
+board_print(board_print([[".",".","4",".",".",".","6","3","."],[".",".",".",".",".",".",".",".","."],["5",".",".",".",".",".",".","9","."],[".",".",".","5","6",".",".",".","."],["4",".","3",".",".",".",".",".","1"],[".",".",".","7",".",".",".",".","."],[".",".",".","5",".",".",".",".","."],[".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".","."]]))
