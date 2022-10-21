9cf313ddd4a8c830a676f4d041d726524655398f
Shiva <shivaheydari.es@gmail.com>
2022-10-20 20:45:23 -0600

    Link List

---
+++ Hash_Link_list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        map_ad={}
+        #key is going to be the address and value is the pos
+        temp=head
+        i=0
+        while temp:
+            if temp not in map_ad:
+                map_ad[temp]=i
+                temp=temp.next
+                i+=1
+            else:
+                return True
+        return False
+
+            
--- Merge_list_h.py
+++ Merge_list_h.py
@@ -1,47 +1,16 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-class Solution:
-    
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+def mergeKLists(lists):
         htemp=head_heap=ListNode()
-        resutl=[]  
-        for k in range(0, len(lists)):
-            ltemp=head_list=lists[k]
-            # if head_heap.next=null:
-            #     head_heap=ltemp
-            if htemp.val >= ltemp.val:
-                #Goes to the left side
-                #finding insertion point
-                inspoint=head_heap
-                while(inspoint.val<ltemp.val):
-                    inspoint=inspoint.next
-                ltemp.next=inspoint.next
-                inspoint.next=ltemp
-            else:
-                #go to the right side
-                #finding the insertion point
-                inspoint=htemp.next
-                while(inspoint.val < ltemp.val):
-                    inspoint=inspoint.next
-                ltemp.next=inspoint.next
-                inspoint.next=ltemp
-            ltemp=ltemp.next 
-         
-            while(head_heap):
-                result.append(head_heap.val)
-                head_heap=head_heap.next
-            return result  
+        print(lists)
+
             
 
-
+# def heap_find(heap, node):
+mergeKLists([1,2,4])
     
-
-
-    
-        #make a heap 
-        # for k in list of list and each element check the heap
