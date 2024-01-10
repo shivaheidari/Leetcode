82175d8a72eb1a16ddb23c47c5bb1271b71a61de
Shiva Heydari <shivaheydari.es@gmail.com>
2024-01-10 14:08:20 -0700

    solution

--- combinatiob_sum.py
+++ combinatiob_sum.py
@@ -1,13 +1,8 @@
 #combination sum: given array of distinct values and target value, find all unique combinations in array where the numbers sum to target value.
-
+from functools import reduce
 candidates = [2,3,6,7]
 target = 7
 result = []
 j = 0
 subres = []
 
-#how many of me
-def summation(subres, )
-
-#for i in range(0, len(candidates)):
-# fo j in range(0, len(candidates))
