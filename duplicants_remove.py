486ee4aa049be6d0acd2dba729330b0413012654
Shiva Heydari <shivaheydari.es@gmail.com>
2025-04-23 11:15:36 -0600

    optim solution

---
+++ duplicants_remove.py
@@ -0,0 +1,29 @@
+"""
+Given a sorted integer array nums, remove duplicates in-place such that duplicates appear at most twice (allow two occurrences of each element). Return the new length 
+of the array.
+Example:
+Input: nums = [1,1,1,2,2,3]
+Output: 5 (because nums = [1,1,2,2,3,_...]; first 5 elements are valid).
+"""
+
+def InplaceRmv(nums):
+    if not nums:
+        return 0
+    i = 0  # Pointer to place the next valid element
+    count = 1  # Count of current element occurrences
+    
+    for j in range(1, len(nums)):
+        if nums[j] == nums[j - 1]:
+            count += 1
+        else:
+            count = 1  # Reset for new element
+        
+        if count <= 2:
+            nums[i] = nums[j]
+            i += 1
+    
+    return i
+
+# Test Cases
+print(InplaceRmv([1, 1, 1, 2, 2, 3]))         # Output: 5 (nums = [1, 1, 2, 2, 3, ...])
+print(InplaceRmv([1, 1, 1, 2, 2, 3, 3, 3]))   # Output: 6 (nums = [1, 1, 2, 2, 3, 3, ...])
