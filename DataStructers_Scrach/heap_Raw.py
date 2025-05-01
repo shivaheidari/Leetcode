90f0954f4946637299aa69d69692081ea9a93e79
Shiva Heydari <shivaheydari.es@gmail.com>
2025-05-01 12:31:09 -0600

    heapyfy

---
+++ DataStructers_Scrach/heap_Raw.py
@@ -0,0 +1,25 @@
+
+"""
+implementing min heap
+
+"""
+
+class myheap:
+    def __init__(self):
+        self.heap = [None]
+        self.size = 0
+
+    def build(self, arr):
+        pass
+
+    def _stif_up(self, i):
+        pass
+
+    def _stif_down(self, i):
+        pass
+    
+    def insert(self, val):
+        pass
+    def pop(self, val):
+        pass
+    
