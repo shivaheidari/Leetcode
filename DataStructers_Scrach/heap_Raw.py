dc4db066b5e3f5207c69d9c55e8145a5a890e05f
Shiva Heydari <shivaheydari.es@gmail.com>
2025-05-02 11:47:30 -0600

    stif up

--- DataStructers_Scrach/heap_Raw.py
+++ DataStructers_Scrach/heap_Raw.py
@@ -22,11 +22,16 @@ class myheap:
 
 
     def _stif_up(self, i):
-        pass
+        parent = (i - 1) // 2
+        if i > 0 and self.heap[i] < self.heap[parent]:
+            self.heap[i], self.heap[parent] = self.heap[parent], self.heap[index]
+            self._stif_up(parent)
+       
 
    
     
     def _stif_down(self, i):
+        #compare left, righ in swap case: stif down swaped child
         left_child = self._get_left_child(i)
         right_child = self._get_right_child(i)
 
@@ -47,8 +52,16 @@ class myheap:
     
     def _get_left_child(self, i):
         return 2 * i + 1
-    def insert(self, val):
-        pass
-    def pop(self, val):
-        pass
     
+    
+    def insert(self, val):
+        self.heap.append(val)
+        self._stif_up(len(self.heap)-1)
+
+    def extract_min(self):
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        
+
+        
+        
+  
