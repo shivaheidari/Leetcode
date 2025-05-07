107efeb284d1269e7860ecee086e9e10d96ebbb5
Shiva Heydari <shivaheydari.es@gmail.com>
2025-05-07 10:13:00 -0600

    init

---
+++ DataStructers_Scrach/double_linked_list.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+    def __repr__(self):
+        return str(self.data)
+    
+
+class Double_linked_list:
+    def __init__(self):
+        
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+    
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+
+    def __repr__(self):
+        nodes = [str(node)for node in self]
+        return "->".join(nodes)+"->None"
+    
+    def append(self, data):
+        #insert at the end
+        node = Node(data)
+        
+        if self.size == 0:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+        self.size += 1
+        
+    def search(self, data):
+        #return true if data is in the list
+        pass
+    def preappend(self, data):
+        #insert at the begining
+        pass
+    def reverse(self):
+        pass
+    
+   
+    def is_empty(self):
+        pass
+    def insert_after(self, target_data, data):
+        pass
+    def delete(self,data):
+        pass
+
+l = Double_linked_list()
+l.append(3)
+l.append(4)
+print(l)
